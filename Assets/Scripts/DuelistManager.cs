72def65c864334d4ac6a5f907369aa46599b53cb
Angel Mavare <angelmavare1@gmail.com>
2022-01-28 15:43:58 -0400

    Add Movement Script to Duelist
    Adding one step per click to character using the button red or blue

--- Assets/Scripts/DuelistManager.cs
+++ Assets/Scripts/DuelistManager.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class DuelistManager : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 40;
     public float dirX;
+    public string character;
     private Rigidbody2D rb2D;
     private bool moveRed;
     private bool moveBlue;
+    //------------------------
+    private Vector2 targetPosition;
+    private Vector2 touchPosition;
+    private int lane = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,36 +24,57 @@ public class DuelistManager : MonoBehaviour
         moveRed = false;
         moveBlue = false;
 
+        
+
     }
 
-    // Update is called once per frame
-    void Update()
+private void Awake()
+{
+    // set these in your Awake():
+    targetPosition = transform.position;
+}
+
+
+
+public void ButtonClick() {
+
+        if (Input.touchCount > 0)
+        {
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                touchPosition = Input.GetTouch(0).position;
+
+                if ( character == "red")
+                lane += 1;
+                if ( character == "blue")
+                lane -= 1;
+
+                lane = Mathf.Clamp(lane, -2, 2); // change to how many lanes you have
+            }
+        }
+
+        //float laneWidth = 10f; // change from 2 if you need to
+        float laneTarget = 0; //lane * laneWidth;
+        if (character == "red") {
+            laneTarget = 50;
+        }
+        else if(character == "blue" ){
+            laneTarget = -50;
+        }
+
+        targetPosition.x = Mathf.MoveTowards(targetPosition.x, laneTarget, speed * Time.deltaTime);
+        float tilt = laneTarget - targetPosition.x;
+    }
+
+    private void Update()
     {
-        //dirX = 
+        
     }
 
-    
-
-    public void ButtonDownRed() {
-        moveRed = true;
-        dirX = speed;
-    }
-    public void ButtonDownBlue()
-    {
-        moveRed = true;
-        dirX = -speed;
-    }
-
-    private void MovementRed()
-    {
-
-        //Output this to console when Button1 or Button3 is clicked
-        Debug.Log("You have clicked the button!");
-
-    }
     private void FixedUpdate()
     {
-        //comments
-        rb2D.velocity = new Vector2(dirX, rb2D.velocity.y);
+        rb2D.MovePosition(targetPosition);
+
     }
 }
